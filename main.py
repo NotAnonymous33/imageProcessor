3d1a8d1e8d140158c48584144f17af9e4a2b4a1f
Ismail <ismail.choudhury@student.manchester.ac.uk>
2023-11-30 13:44:07 +0000

    add gaussian blur

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def greyscale(image):
-    # go through each pixel and change it to black and white
+    # Go through each pixel and change it to black and white
     for y in range(image.height):
         for x in range(image.width):
             r, g, b = image.getpixel((x, y))
@@ -42,6 +42,53 @@ def linear_blur(image, strength):
     return Image.fromarray(output_array)
 
 
+def gaussian_kernel(size, sigma):
+    # Create a Gaussian kernel
+    kernel = np.zeros((size, size))
+    for x in range(size):
+        for y in range(size):
+            kernel[x, y] = np.exp(-((x - size // 2) ** 2 + (y - size // 2) ** 2) / (2 * sigma ** 2))
+    return kernel / np.sum(kernel)
+
+
+def gaussian_blur(image, strength, sigma):
+    # Convert PIL image to numpy array
+    img_array = np.array(image)
+    height = image.height
+    width = image.width
+
+    strength = int(strength * min(height, width))
+
+    kernel_size = 2 * strength + 1
+    kernel = gaussian_kernel(kernel_size, sigma)
+
+    # Initialize the output array
+    output_array = np.zeros_like(img_array)
+
+    # Apply Gaussian blur
+    for y in range(height):
+        for x in range(width):
+            x_min = max(0, x - strength)
+            x_max = min(width, x + strength + 1)
+            y_min = max(0, y - strength)
+            y_max = min(height, y + strength + 1)
+
+            surrounding_box = img_array[y_min:y_max, x_min:x_max]
+
+            kernel_y_min = max(0, strength - y)
+            kernel_y_max = kernel_size - max(0, strength + y + 1 - height)
+            kernel_x_min = max(0, strength - x)
+            kernel_x_max = kernel_size - max(0, strength + x + 1 - width)
+
+            relevant_kernel = kernel[kernel_y_min:kernel_y_max, kernel_x_min:kernel_x_max]
+
+            for c in range(3):
+                output_array[y, x, c] = np.sum(surrounding_box[:, :, c] * relevant_kernel) / np.sum(relevant_kernel)
+
+    return Image.fromarray(output_array.astype(np.uint8))
+
+
 input_image = Image.open('images/butterfly.jpg')
-output_image = linear_blur(input_image, 0.1)
+# output_image = linear_blur(input_image, 0.1)
+output_image = gaussian_blur(input_image, 0.4, 10)
 output_image.save('output.jpg')
